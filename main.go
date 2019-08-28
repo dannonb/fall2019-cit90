0582534b4bfe5b1db606e7a3bb14ca6265721f37
Destiny Malone <destinymalone@Destinys-MacBook-Pro.local>
2019-08-28 15:02:40 -0700

    homework assignments exercise 1 2 and 3

---
+++ main.go
@@ -0,0 +1,27 @@
+package main 
+
+import "fmt"
+
+var a string 
+var b int 
+var c bool
+
+
+
+func main() {
+	x := 42
+	y := "james bond"
+	z := true
+	fmt.Printf("%v wears the number %v and is %v\n", y, x, z)
+	fmt.Println(x)
+	fmt.Println(y)
+	fmt.Println(z)
+
+	fmt.Println(a)
+	fmt.Println(b)
+	fmt.Println(c)
+	
+
+	
+	
+}
