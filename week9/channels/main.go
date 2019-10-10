c10ce0046a20413af0e84add1e69d1c7f7bbba83
dannonb <dannonbigham@gmail.com>
2019-10-09 21:28:10 -0700

    wk9

---
+++ week9/channels/main.go
@@ -0,0 +1,21 @@
+package main 
+
+import ("fmt")
+
+func main() {
+	const gs = 100
+	counter := 0
+
+	c := make(chan int)
+
+	for i := 0; i < gs; i++ {
+		go func() {
+			c <- 1
+		}()
+	}
+
+	for i := 0; i < gs; i++ {
+		counter += <-c
+	}
+	fmt.Println("total counter:", counter)
+}
